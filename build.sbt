ea6f27aed91123ff7ef3d730c9b24eae39688465
Muqiu Han <muqiu-han@outlook.com>
2024-05-08 22:40:50 +0800

    show demo window at cursor point

--- build.sbt
+++ build.sbt
@@ -5,8 +5,9 @@ lazy val root = project
   .settings(
     name := "maslation",
     version := "0.1.0-SNAPSHOT",
-
     scalaVersion := scala3Version,
-
-    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
+    libraryDependencies ++= Seq(
+      "org.scalameta" %% "munit" % "0.7.29" % Test,
+      "org.scala-lang.modules" %% "scala-swing" % "3.0.0"
+    )
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,20 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import java.awt.MouseInfo
+import java.awt.Point
+import scala.swing._
 
-def msg = "I was compiled by Scala 3. :)"
+@main def hello(): Unit =
+  new Frame:
+    title = "Hello world"
+    location =
+      val location = MouseInfo.getPointerInfo.getLocation
+      Point(location.x, location.y)
+
+    contents = new FlowPanel:
+      contents += new Label("Launch rainbows:")
+      contents += new Button("Click me"):
+        reactions += { case event.ButtonClicked(_) =>
+          println("All the colours!")
+        }
+    
+    pack()
+    open()
