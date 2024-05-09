772d41392f7449df917c732f370fd0ff45067d02
Muqiu Han <muqiu-han@outlook.com>
2024-05-09 21:55:36 +0800

    [update]: view.Error

--- src/main/scala/com/muqiuhan/maslation/view/Error.scala
+++ src/main/scala/com/muqiuhan/maslation/view/Error.scala
@@ -9,17 +9,14 @@ object Error extends Dialog:
   resizable = false
   visible = true
 
-  def stackTraceElementsToString(exn: Exception): String =
-    exn.getStackTrace.map(_.toString()).reduce((s1: String, s2: String) => s1.concat("\n").concat(s2))
+  def stackTraceElementsToString(exn: Exception): String = exn.getStackTrace.map(_.toString())
+    .reduce((s1: String, s2: String) => s1.concat("\n").concat(s2))
 
   def apply(info: String, details: Option[String] = None): Unit =
     val exn = RuntimeException(info)
     contents = new FlowPanel():
       contents += component.TextArea(info, Some(Color.RED))
-      contents += component.TextArea(
-        details.getOrElse(stackTraceElementsToString(exn)),
-        Some(Color.GRAY)
-      )
+      contents += component.TextArea(details.getOrElse(stackTraceElementsToString(exn)), Some(Color.GRAY))
 
     centerOnScreen()
     pack()
