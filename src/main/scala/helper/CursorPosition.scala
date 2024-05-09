5ddf0ba2275fc7be40ed55cdd76eefefea48da54
Muqiu Han <muqiu-han@outlook.com>
2024-05-09 09:49:25 +0800

    add CursorPosition helper class

---
+++ src/main/scala/helper/CursorPosition.scala
@@ -0,0 +1,9 @@
+package helper
+
+import java.awt.Point
+import java.awt.MouseInfo
+
+object CursorPosition:
+  def apply() : Point =
+    val location = MouseInfo.getPointerInfo.getLocation
+    Point(location.x, location.y)
