2cfd27953a56cf6cfbc477c8145b346257e2e563
Muqiu Han <muqiu-han@outlook.com>
2024-05-10 09:37:14 +0800

    [update]: helper.BestDimension

---
+++ src/main/scala/com/muqiuhan/maslation/helper/BestDimension.scala
@@ -0,0 +1,21 @@
+package com.muqiuhan.maslation.helper
+
+import java.awt.Toolkit
+import scala.swing.Label
+
+object BestDimension:
+  private val screenSize = Toolkit.getDefaultToolkit.getScreenSize
+  private val defaultSize = new Label().font.getSize
+
+  inline def height(itemSize: Int = defaultSize, h : Int): Int =
+    if h * itemSize > screenSize.height then
+      screenSize.height / 5 / defaultSize
+    else
+      h + 1
+
+  inline def width(itemSize: Int = defaultSize, w : Int): Int =
+    val bestWidth = screenSize.width / 5 / defaultSize
+    if w > bestWidth then
+      bestWidth
+    else
+      w + 1
