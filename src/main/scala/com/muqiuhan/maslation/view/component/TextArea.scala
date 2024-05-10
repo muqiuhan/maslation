65aaf6e93fb851c222831c7f204b797dbaa95eb0
Muqiu Han <muqiu-han@outlook.com>
2024-05-10 09:37:58 +0800

    [add]: ScrollPane to view.component.TextArea

--- src/main/scala/com/muqiuhan/maslation/view/component/TextArea.scala
+++ src/main/scala/com/muqiuhan/maslation/view/component/TextArea.scala
@@ -1,18 +1,39 @@
 package com.muqiuhan.maslation.view.component
 
 import com.formdev.flatlaf.ui.FlatBorder
+import com.muqiuhan.maslation.helper
 
-import java.awt.{Color, Dimension}
-import java.util.Comparator
+import java.awt.Color
 import scala.swing
+import scala.swing.ScrollPane
 
-class TextArea(text: String, color: Option[Color] = None) extends swing.TextArea(text):
+class TextArea(text: String, color: Option[Color] = None)
+    extends ScrollPane(
+      new swing.TextArea(
+        text,
+        rows0 = helper.BestDimension.height(h = text.split("\\R").length),
+        columns0 = helper
+          .BestDimension
+          .width(w =
+            text
+              .split("\\R")
+              .reduceLeft((s1, s2) =>
+                if s1.length > s2.length then
+                  s1
+                else
+                  s2
+              )
+              .length
+          )
+      ):
+        color match
+          case None =>
+            ()
+          case Some(color) =>
+            foreground = color
 
-  color match
-    case None        => ()
-    case Some(color) => foreground = color
-
-  editable = false
-  lineWrap = true
-  wordWrap = true
-  border = FlatBorder()
+        editable = false
+        lineWrap = true
+        wordWrap = true
+        border = FlatBorder()
+    )
